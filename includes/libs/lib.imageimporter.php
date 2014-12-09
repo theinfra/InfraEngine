d8b2856ded06b182d7400d19990893c485518c46
infra <theinfra@users.noreply.github.com>
2014-12-09 06:04:47 +0000

    LIBS Agrego base para libreria ImageImporter para importar y transformar imagenes

---
+++ includes/libs/lib.imageimporter.php
@@ -0,0 +1,26 @@
+<?php
+
+class APPLIB_IMAGEIMPORTER {
+	
+	private $basefile = "";
+	private $filename ="";
+	
+	function loadFile($BaseFilePath, $BaseFileName){
+		if(!file_exists($BaseFilePath)){
+			return false;
+		}
+		
+		$this->basefile = $BaseFilePath; 
+		$this->filename = $BaseFileName;
+
+		return true;
+	}
+	
+	function copyOrig($destination){
+		if(!is_dir($destination)){
+			app_mkdir($destination, "0644", true);
+		}
+		
+		copy($this->basefile, $destination.DIRECTORY_SEPARATOR.$this->filename);
+	}
+}
