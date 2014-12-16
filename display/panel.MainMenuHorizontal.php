fd926c0a085bd062a398aafff96c9655ac078bc3
infra <theinfra@users.noreply.github.com>
2014-12-16 01:29:52 +0000

    Quito los elementos de menu de TMC, solo dejo inicio

--- display/panel.MainMenuHorizontal.php
+++ display/panel.MainMenuHorizontal.php
@@ -5,21 +5,6 @@ class PANEL_MAINMENUHORIZONTAL extends AppPanel {
 	public function SetPanelSettings(){
 		$menu_items = array(
 			"Home" => "index",
-			"Reserve" => array(
-				"All" => "item",
-				"Space" => "item/espacio",
-				"Equipment" => "item/equipo",
-				"Addon" => "item/addon",
-				),
-			"MyReserves" => "reserve",
-			"MyAccount" => "user",
-			"Admin" => array(
-				"Reserves" => "reserve/admin",
-				"Items" => "item/admin",
-				"Users" => "user/admin",
-				"Reports" => "reports",
-				"Settings" => "settings",
-			),
 		);
 		
 		if(getUserData()){
