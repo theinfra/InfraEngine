7cf5757a5bf01834b6d44acaad47e61c3f16de2f
infra <theinfra@users.noreply.github.com>
2014-12-19 02:22:45 +0000

    CONFIGURACION INICIAL Agrego un query de insert con un usuario admin inicial para que pueda hacer autenticacion al principio

--- config/config.php
+++ config/config.php
@@ -12,3 +12,7 @@ $GLOBALS['APP_CONFIG']['db_pwd'] = 'apppwd';
 $GLOBALS['APP_CONFIG']['db_name'] = 'app_db';
 
 $GLOBALS['APP_CONFIG']['CharacterSet'] = 'UTF-8';
+
+/*
+ * INSERT INTO `usuario` (`firstname`, `lastname`, `mail`, `username`, `password`, `salt`, `phone`, `status`, `membershiptype`, `usergroup`) VALUES ('Admin', 'Admin', 'admin@localhost', 'admin', '84b5a4a7118f06dd7c9713c514b328f56bcc564024d5de94cff682850bed2c914221c49f1b0a1ae40d183c00878b9ed57a579a47486bf8b2fcce7d850d531729', '3a287ab2b87e0e99', '0', 2, 0, 3);
+*/
