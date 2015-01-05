e24b0b40c1fae2b73de6e9c8573450cc3898c192
infra <theinfra@users.noreply.github.com>
2015-01-05 01:25:23 +0000

    INSTALACION INICIAL Cambio que si prenda la db por default, y cambio el password por default del usuario admin a "aaa" con la longitud apropiada

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ $GLOBALS['APP_CONFIG']['HidePHPErrors'] = false;
 
 $GLOBALS['APP_CONFIG']['language'] = 'es';
 
-$GLOBALS['APP_CONFIG']['nodb'] = true;
+$GLOBALS['APP_CONFIG']['nodb'] = false;
 $GLOBALS['APP_CONFIG']['db_host'] = 'localhost';
 $GLOBALS['APP_CONFIG']['db_user'] = 'app_user';
 $GLOBALS['APP_CONFIG']['db_pwd'] = 'apppwd';
@@ -14,5 +14,5 @@ $GLOBALS['APP_CONFIG']['db_name'] = 'app_db';
 $GLOBALS['APP_CONFIG']['CharacterSet'] = 'UTF-8';
 
 /*
- * INSERT INTO `usuario` (`firstname`, `lastname`, `mail`, `username`, `password`, `salt`, `phone`, `status`, `membershiptype`, `usergroup`) VALUES ('Admin', 'Admin', 'admin@localhost', 'admin', '84b5a4a7118f06dd7c9713c514b328f56bcc564024d5de94cff682850bed2c914221c49f1b0a1ae40d183c00878b9ed57a579a47486bf8b2fcce7d850d531729', '3a287ab2b87e0e99', '0', 2, 0, 3);
+ * INSERT INTO `usuario` (`firstname`, `lastname`, `mail`, `username`, `password`, `salt`, `phone`, `status`, `membershiptype`, `usergroup`) VALUES ('Admin', 'Admin', 'admin@localhost', 'admin', '5d38559629b3a4241e77d09699e4fe8f5580eebed5a18593f97e053c40f58183edda8a2de0dcec1e9fbeb7a65b6139a583d8599022f10640489632b088a749d1', '3a287ab2b87e0e99', '0', 2, 0, 3);
 */
