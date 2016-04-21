09872c48673db43d5caeff3c98f7571b50de7d15
infra <theinfra@users.noreply.github.com>
2016-04-21 22:15:21 +0000

    JAVASCRIPT Agrego funcion para checar si es un int

--- javascript/views.js
+++ javascript/views.js
@@ -53,3 +53,12 @@ function EnableFormElements(classname){
 	
 	$("."+classname+" td input, ."+classname+" td select").removeAttr('disabled');
 }
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
